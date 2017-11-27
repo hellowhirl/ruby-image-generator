a17ce47db4d7f38bc1960e7000db48b80c871cb4
tomholford <tomholford0@gmail.com>
2017-11-26 17:39:56 -0800

    Format multiline string, add example to README

--- image_generator.rb
+++ image_generator.rb
@@ -1,5 +1,12 @@
 input = %w{ Apple Banana Cherry Dragonfruit Eggfruit Fig Grapefruit Honeydew }
 
 input.each_with_index do |val, index|
-  puts %Q(<li><a href="img/img_#{index+1}.jpg" title="#{val}"><img src="img/img_#{index+1}.jpg" alt="#{val}"><span>#{val}</span></a></li>)
+  puts %Q(
+    <li>
+      <a href="img/img_#{index}.jpg" title="#{val}">
+        <img src="img/img_#{index}.jpg" alt="#{val}">
+        <span>#{val}</span>
+      </a>
+    </li>
+  )
 end
