5ac4d3b7145c12243114d1f55a9dea9e6eabbc0c
hellowhirl <anthony.p.avila@gmail.com>
2017-11-27 10:27:36 +0900

    modify for general use

--- image_generator.rb
+++ image_generator.rb
@@ -1,8 +1,5 @@
-input = %w{ 外観写真① エントランス① エントランス② エントランス③ 外観写真② 外観写真③ 前面道路 キッチン リビング 浴室 }
+input = %w{ Apple Banana Cherry Dragonfruit Eggfruit Fig Grapefruit Honeydew }
 
-input.each_with_index {|val, index| puts %Q(<li><a href="img/img_#{index+1}.jpg" rel="slide_b" class="fancy_thum" title="#{val}"><img src="img/img_#{index+1}.jpg" alt="マンション名 #{val}"><span>#{val}</span></a></li>) }
-
-puts %Q{</ul>}
-puts %Q{<ul class="slider slider-nav">}
-
-input.each_with_index {|val, index| puts %Q(<li><a href="img/img_#{index+1}.jpg" rel="slide_s" class="fancy_thum" title="#{val}"><img src="img/img_#{index+1}.jpg" alt="マンション名 #{val}"><span>#{val}</span></a></li>) }
+input.each_with_index do |val, index|
+  puts %Q(<li><a href="img/img_#{index+1}.jpg" title="#{val}"><img src="img/img_#{index+1}.jpg" alt="#{val}"><span>#{val}</span></a></li>)
+end
